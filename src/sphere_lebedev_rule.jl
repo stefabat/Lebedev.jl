c69b27cba12b539d7529c6a7c542530f4740be8a
Stefano Battaglia <battaglia1803@gmail.com>
2020-12-27 23:51:31 +0100

    added ldxxxx functions for 6 and 14 points

--- src/sphere_lebedev_rule.jl
+++ src/sphere_lebedev_rule.jl
@@ -171,24 +171,146 @@ end
 
 
 """
+    function ld0006!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+
+Compute the 6 point Lebedev angular grid.
+"""
+function ld0006!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+  a = 0.0
+  b = 0.0
+  n = 0
+
+  v  = 0.1666666666666667
+  n += gen_oh!(1, a, b, v, n, x, y, z, w)
+  
+  return nothing
+end
+
+
+"""
+    function ld0014!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+
+Compute the 14 point Lebedev angular grid.
+"""
+function ld0014!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+  a = 0.0
+  b = 0.0
+  n = 0
+
+  v  = 0.6666666666666667e-1
+  n += gen_oh!(1, a, b, v, n, x, y, z, w)
+  v  = 0.7500000000000000e-1
+  n += gen_oh!(3, a, b, v, n, x, y, z, w)
+
+  return nothing
+end
+
+
+"""
     function ld0026!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
 
 Compute the 26 point Lebedev angular grid.
 """
 function ld0026!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
-  a = 0.0
-  b = 0.0
-  n = 0
+    a = 0.0
+    b = 0.0
+    n = 0
 
-  v  = 0.4761904761904762e-1
-  n += gen_oh!(1, a, b, v, n, x, y, z, w)
-  v  = 0.3809523809523810e-1
-  n += gen_oh!(2, a, b, v, n, x, y, z, w)
-  v  = 0.3214285714285714e-1
-  n += gen_oh!(3, a, b, v, n, x, y, z, w)
-  n  = n - 1
+    v  = 0.4761904761904762e-1
+    n += gen_oh!(1, a, b, v, n, x, y, z, w)
+    v  = 0.3809523809523810e-1
+    n += gen_oh!(2, a, b, v, n, x, y, z, w)
+    v  = 0.3214285714285714e-1
+    n += gen_oh!(3, a, b, v, n, x, y, z, w)
+    
+    return nothing
 end
 
+
+"""
+    function ld0590!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+    
+Compute the 590 point Lebedev angular grid.
+"""
+function ld0590!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
+    a = 0.0
+    b = 0.0
+    n = 0
+    
+    v  = 0.3095121295306187e-3
+    n += gen_oh!(1, a, b, v, n, x, y, z, w)
+    v  = 0.1852379698597489e-2
+    n += gen_oh!(3, a, b, v, n, x, y, z, w)
+    a  = 0.7040954938227469
+    v  = 0.1871790639277744e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.6807744066455243
+    v  = 0.1858812585438317e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.6372546939258752
+    v  = 0.1852028828296213e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.5044419707800358
+    v  = 0.1846715956151242e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.4215761784010967
+    v  = 0.1818471778162769e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.3317920736472123
+    v  = 0.1749564657281154e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.2384736701421887
+    v  = 0.1617210647254411e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.1459036449157763
+    v  = 0.1384737234851692e-2
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.6095034115507196e-1
+    v  = 0.9764331165051050e-3
+    n += gen_oh!(4, a, b, v, n, x, y, z, w)
+    a  = 0.6116843442009876
+    v  = 0.1857161196774078e-2
+    n += gen_oh!(5, a, b, v, n, x, y, z, w)
+    a  = 0.3964755348199858
+    v  = 0.1705153996395864e-2
+    n += gen_oh!(5, a, b, v, n, x, y, z, w)
+    a  = 0.1724782009907724
+    v  = 0.1300321685886048e-2
+    n += gen_oh!(5, a, b, v, n, x, y, z, w)
+    a  = 0.5610263808622060
+    b  = 0.3518280927733519
+    v  = 0.1842866472905286e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    a  = 0.4742392842551980
+    b  = 0.2634716655937950
+    v  = 0.1802658934377451e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    a  = 0.5984126497885380
+    b  = 0.1816640840360209
+    v  = 0.1849830560443660e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    a  = 0.3791035407695563
+    b  = 0.1720795225656878
+    v  = 0.1713904507106709e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    a  = 0.2778673190586244
+    b  = 0.8213021581932511e-1
+    v  = 0.1555213603396808e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    a  = 0.5033564271075117
+    b  = 0.8999205842074875e-1
+    v  = 0.1802239128008525e-2
+    n += gen_oh!(6, a, b, v, n, x, y, z, w)
+    
+    return nothing
+end
+
+
+"""
+    function lebedev(n::Integer)
+
+Compute the `n` point Lebedev angular grid.
+"""
 function lebedev(n::Integer)
 
     # initialize arrays before checking `n` is valid just to avoid repeating
@@ -198,8 +320,14 @@ function lebedev(n::Integer)
     z = zeros(n)
     w = zeros(n)
     
-    if n == 26
+    if n == 6
+        ld0006!(x, y, z, w)
+    elseif n == 14
+        ld0014!(x, y, z, w)
+    elseif n == 26
         ld0026!(x, y, z, w)
+    elseif n == 590
+        ld0590!(x, y, z, w)
     else
         error("n is not a valid number of points")
     end
@@ -207,104 +335,3 @@ function lebedev(n::Integer)
     return x,y,z,w
 end
 
-
-"""
-function ld0590!(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, w::Vector{Float64})
-    
-    Compute the 590 point Lebedev angular grid.
-    
-    Modified:
-    
-    12 September 2010
-    
-    Author:
-    
-    Dmitri Laikov
-    
-    Reference:
-    
-Vyacheslav Lebedev, Dmitri Laikov,
-A quadrature formula for the sphere of the 131st
-algebraic order of accuracy,
-Russian Academy of Sciences Doklady Mathematics,
-Volume 59, Number 3, 1999, pages 477-481.
-
-Parameters:
-
-Output, double X[N], Y[N], Z[N], W[N], the coordinates
-and weights of the points.
-"""
-function ld0590!(x::AbstractVector, y::AbstractVector, z::AbstractVector, w::AbstractVector)
-    
-    # initialize variables
-    a = 0.0
-    b = 0.0
-    n = 0
-    
-    v = 0.3095121295306187e-3
-    n = n + gen_oh( 1, a, b, v, x + n, y + n, z + n, w + n )
-    v = 0.1852379698597489e-2
-    n = n + gen_oh( 3, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.7040954938227469
-    v = 0.1871790639277744e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.6807744066455243
-    v = 0.1858812585438317e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.6372546939258752
-    v = 0.1852028828296213e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.5044419707800358
-    v = 0.1846715956151242e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.4215761784010967
-    v = 0.1818471778162769e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.3317920736472123
-    v = 0.1749564657281154e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.2384736701421887
-    v = 0.1617210647254411e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.1459036449157763
-    v = 0.1384737234851692e-2
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.6095034115507196e-1
-    v = 0.9764331165051050e-3
-    n = n + gen_oh( 4, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.6116843442009876
-    v = 0.1857161196774078e-2
-    n = n + gen_oh( 5, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.3964755348199858
-    v = 0.1705153996395864e-2
-    n = n + gen_oh( 5, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.1724782009907724
-    v = 0.1300321685886048e-2
-    n = n + gen_oh( 5, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.5610263808622060
-    b = 0.3518280927733519
-    v = 0.1842866472905286e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.4742392842551980
-    b = 0.2634716655937950
-    v = 0.1802658934377451e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.5984126497885380
-    b = 0.1816640840360209
-    v = 0.1849830560443660e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.3791035407695563
-    b = 0.1720795225656878
-    v = 0.1713904507106709e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.2778673190586244
-    b = 0.8213021581932511e-1
-    v = 0.1555213603396808e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    a = 0.5033564271075117
-    b = 0.8999205842074875e-1
-    v = 0.1802239128008525e-2
-    n = n + gen_oh( 6, a, b, v, x + n, y + n, z + n, w + n )
-    n = n - 1
-    
-end
